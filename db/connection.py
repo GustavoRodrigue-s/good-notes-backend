a20d6b6247a379599fb6d7a3cc6be2193ed934b5
Gustavo Rodrigues <gustavo.fabiano.2010@hotmail.com>
2022-02-25 19:51:20 -0300

    dawd

--- db/connection.py
+++ db/connection.py
@@ -1,26 +1,23 @@
 import sqlite3
 
 def connectionDB(functionToExecute, connectionConfig):
-   try:
-      connectionDatabase = sqlite3.connect('../db/users.db')
-      cursor = connectionDatabase.cursor()
+   connectionDatabase = sqlite3.connect('../db/users.db')
+   cursor = connectionDatabase.cursor()
 
-      print('barbearia home')
+   print('barbearia home')
 
-      if connectionConfig['toAddUser']:
-         cursor.execute(functionToExecute[0], functionToExecute[1])
-      else:
-         cursor.execute(functionToExecute)
+   if connectionConfig['toAddUser']:
+      cursor.execute(functionToExecute[0], functionToExecute[1])
+   else:
+      cursor.execute(functionToExecute)
 
-      if connectionConfig['getAllUsers']:
-         data = cursor.fetchall()
-      else:
-         data = cursor.fetchone()
+   if connectionConfig['getAllUsers']:
+      data = cursor.fetchall()
+   else:
+      data = cursor.fetchone()
 
-      connectionDatabase.commit()
-      cursor.close()
-      connectionDatabase.close()
+   connectionDatabase.commit()
+   cursor.close()
+   connectionDatabase.close()
 
-      return data
-   except sqlite3.Error as e:
-      print(e)
+   return data
--- db/createDatabase.py
+++ db/createDatabase.py
@@ -1,21 +1,18 @@
 import sqlite3
 
-try:
-   connectionDatabase = sqlite3.connect('users.db')
-   cursor = connectionDatabase.cursor()
+connectionDatabase = sqlite3.connect('users.db')
+cursor = connectionDatabase.cursor()
 
-   cursor.execute('''create table users (
-      username text,
-      email text,
-      password text,
-      id text,
-      apiKey text
-   )''')
+cursor.execute('''create table users (
+   username text,
+   email text,
+   password text,
+   id text,
+   apiKey text
+)''')
 
-   print('criou o barato')
+print('criou o barato')
 
-   connectionDatabase.commit()
-   cursor.close()
-   connectionDatabase.close()
-except sqlite3.Error as e:
-   print(e)
+connectionDatabase.commit()
+cursor.close()
+connectionDatabase.close()
